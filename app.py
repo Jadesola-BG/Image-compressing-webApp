96b8aedcb952834f8b02359ce4e9f3049e29bfc4
Jadesola-BG <jadesola.adedokun@babbangona.com>
2022-04-26 01:11:33 +0100

    image compression operation

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
+from flask import Flask, render_template, request, redirect, url_for, flash, send_file
 from werkzeug.utils import secure_filename
 from compress import compress_image
 
@@ -20,45 +20,47 @@ def allowed_file(filename):
 def too_large(e):
     return "File is too large", 413
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
     
+def clean_uploadfolder():
+    imagelist = [img for img in os.listdir(app.config['UPLOAD_FOLDER']) if not img.startswith("icon")]
+    for images in imagelist:
+        os.remove(os.path.join(app.config['UPLOAD_FOLDER'], images))
+    
 
 @app.route('/', methods=['POST'])
 def image_upload():
+
     if "uploaded_image" not in request.files:
         flash("No file part")
         return redirect(request.url)
 
     image = request.files['uploaded_image']
-
     if image.filename == '':
         flash('No image selected for uploading')
         return redirect(request.url)
 
     if image and allowed_file(image.filename):
+        clean_uploadfolder()
         image_name = secure_filename(image.filename)
         image.save(os.path.join(app.config['UPLOAD_FOLDER'], image_name))
-        flash('Image successfully uploaded !!! ')
-        flash("COMPRESSING IMAGE !!!")
-        
+        flash('Image successfully uploaded & Compressing !!! ')
+        #image compressing operation
         results = compress_image(image_name, app.config['UPLOAD_FOLDER'])
-
-        
         return render_template('display.html', results = results)
-
+        
     else:
         flash('Please ensure you uploaded only the allowed file types.')
         return redirect(request.url)
-        #return "Invalid image", 400
-    #removing uploaded image
-    os.remove(image_name)
-    return render_template("home.html")
 
 
-
+@app.route('/<filename>', methods=['GET','POST'])
+def download_image(filename):
+    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    return send_file(path, as_attachment=True)
+    
 
 if __name__  == '__main__':
     app.run(debug=True)
--- compress.py
+++ compress.py
@@ -1,11 +1,17 @@
 import os
 from PIL import Image
 
+def determine_image_size(folder, image_name):
+    return os.path.getsize(os.path.join(folder,image_name))
+
+def open_image(folder, image_name):
+    return Image.open(os.path.join(folder,image_name))
+
 def compress_image(image_name, results_folder):
-    old_image = Image.open(os.path.join(results_folder,image_name))
-    old_image_size = os.path.getsize(os.path.join(results_folder,image_name))
-    old_image.save(os.path.join(results_folder, "compressed_" + image_name), optimize=True, quality=65)
-    compressed_image = Image.open(os.path.join(results_folder,  "compressed_" + image_name))
-    compressed_image_size = os.path.getsize(os.path.join(results_folder,  "compressed_" + image_name))
-    return [image_name, compressed_image_size, old_image_size]
+    original_image = open_image(results_folder,image_name)
+    original_image_size = determine_image_size(results_folder, image_name)  
+    original_image.save(os.path.join(results_folder, "compressed_" + image_name), optimize=True, quality=65)
+    compressed_image = open_image(results_folder,  "compressed_" + image_name)
+    compressed_image_size = determine_image_size(results_folder,  "compressed_" + image_name)   
+    return [image_name, compressed_image_size, original_image_size]
     
